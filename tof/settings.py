818113b424c65e422d0459e31f4e8d9b44d419ad
Emmanuel-Aggrey <aggrey.en@gmail.com>
2020-05-27 01:42:07 +0000

    added postgress

--- tof/settings.py
+++ tof/settings.py
@@ -90,13 +90,25 @@ CSRF_COOKIE_SECURE =True
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tree-of-life',
+        'USER': 'tree_of_life',
+        'PASSWORD': 'christme',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
